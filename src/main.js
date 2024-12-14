0522f6607bfc6cb5a384c2c7e35f8d86714447d6
Jean do Nascimento <jeandonascmento@gmail.com>
2024-12-13 23:51:26 -0300

    ultimo commit

--- src/main.js
+++ src/main.js
@@ -1,6 +1,33 @@
 document.addEventListener('DOMContentLoaded' , function() {
     const buttons = document.querySelectorAll('[data-tab-button]');
+    const questions = document.querySelectorAll('[data-faq-question]')
 
+    const heroSection = document.querySelector('.hero');
+    const heroHeight = heroSection.clientHeight;
+
+
+    //hiding navbar
+    window.addEventListener('scroll', function() {
+        const currentPosition = window.scrollY;
+
+        if (currentPosition < heroHeight) {
+            hideHeader();
+        }else{
+            showHeader();
+        }
+    })
+
+    function hideHeader () {
+        const header = document.querySelector('.header');
+        header.classList.add('header--is-hidden')
+    }
+
+    function showHeader () {
+        const header = document.querySelector('.header');
+        header.classList.remove('header--is-hidden')
+    }
+
+    //shows tab
     for (let i = 0; i < buttons.length; i++) {
         buttons[i].addEventListener('click', function (button) {
             const targetTab = button.target.dataset.tabButton;
@@ -11,6 +38,10 @@ document.addEventListener('DOMContentLoaded' , function() {
             removeActiveButton();
             button.target.classList.add('shows__tabs__button--is-active')
         })
+    }
+
+    for (let i = 0; i < questions.length; i++) {
+        questions[i].addEventListener('click', toggleAnswer);
     }
 })
 
@@ -29,4 +60,13 @@ function hideAllTabs () {
     for (let i = 0; i < tabsContainer.length; i++) {
         tabsContainer[i].classList.remove('shows__list--is-active')
     }
+}
+
+
+//FAQ
+function toggleAnswer (element) {
+    const classItem = 'faq__questions__item--is-open';
+    const parentElement = element.target.parentNode;
+
+    parentElement.classList.toggle(classItem);
 }
